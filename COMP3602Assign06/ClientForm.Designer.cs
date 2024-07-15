a91717c3b8cfeaca532baf429ab7952d3066ff04
Alex Wang <44349923+wza2479110@users.noreply.github.com>
2024-07-14 18:46:35 -0700

    Project: C#
    This folder contains one of the project of the C# course work

---
+++ COMP3602Assign06/ClientForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace COMP3602Assign06
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox_CreditHold = new System.Windows.Forms.CheckBox();
+            this.textBox_Notes = new System.Windows.Forms.TextBox();
+            this.Noteslabel = new System.Windows.Forms.Label();
+            this.textBox_YTDSales = new System.Windows.Forms.TextBox();
+            this.YTDSaleslabel = new System.Windows.Forms.Label();
+            this.textBox_PostalCode = new System.Windows.Forms.TextBox();
+            this.PostalCodelabel = new System.Windows.Forms.Label();
+            this.textBox_Province = new System.Windows.Forms.TextBox();
+            this.Provincelabel = new System.Windows.Forms.Label();
+            this.textBox_City = new System.Windows.Forms.TextBox();
+            this.textBox_Address2 = new System.Windows.Forms.TextBox();
+            this.textBox_Address1 = new System.Windows.Forms.TextBox();
+            this.textBox_CompanyName = new System.Windows.Forms.TextBox();
+            this.textBox_ClientCode = new System.Windows.Forms.TextBox();
+            this.CampanyNameLabel = new System.Windows.Forms.Label();
+            this.CityLabel = new System.Windows.Forms.Label();
+            this.Address2Label = new System.Windows.Forms.Label();
+            this.Address1Label = new System.Windows.Forms.Label();
+            this.ClientCodeLabel = new System.Windows.Forms.Label();
+            this.ok_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBox_CreditHold
+            // 
+            this.checkBox_CreditHold.AutoSize = true;
+            this.checkBox_CreditHold.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox_CreditHold.Location = new System.Drawing.Point(120, 381);
+            this.checkBox_CreditHold.Name = "checkBox_CreditHold";
+            this.checkBox_CreditHold.Size = new System.Drawing.Size(90, 21);
+            this.checkBox_CreditHold.TabIndex = 62;
+            this.checkBox_CreditHold.Text = "CreditHold";
+            this.checkBox_CreditHold.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox_CreditHold.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Notes
+            // 
+            this.textBox_Notes.Location = new System.Drawing.Point(120, 338);
+            this.textBox_Notes.Name = "textBox_Notes";
+            this.textBox_Notes.Size = new System.Drawing.Size(100, 23);
+            this.textBox_Notes.TabIndex = 61;
+            // 
+            // Noteslabel
+            // 
+            this.Noteslabel.AutoSize = true;
+            this.Noteslabel.Location = new System.Drawing.Point(12, 338);
+            this.Noteslabel.Name = "Noteslabel";
+            this.Noteslabel.Size = new System.Drawing.Size(46, 17);
+            this.Noteslabel.TabIndex = 60;
+            this.Noteslabel.Text = "Notes:";
+            // 
+            // textBox_YTDSales
+            // 
+            this.textBox_YTDSales.Location = new System.Drawing.Point(120, 296);
+            this.textBox_YTDSales.Name = "textBox_YTDSales";
+            this.textBox_YTDSales.Size = new System.Drawing.Size(100, 23);
+            this.textBox_YTDSales.TabIndex = 59;
+            // 
+            // YTDSaleslabel
+            // 
+            this.YTDSaleslabel.AutoSize = true;
+            this.YTDSaleslabel.Location = new System.Drawing.Point(12, 296);
+            this.YTDSaleslabel.Name = "YTDSaleslabel";
+            this.YTDSaleslabel.Size = new System.Drawing.Size(64, 17);
+            this.YTDSaleslabel.TabIndex = 58;
+            this.YTDSaleslabel.Text = "YTDSales:";
+            // 
+            // textBox_PostalCode
+            // 
+            this.textBox_PostalCode.Location = new System.Drawing.Point(120, 257);
+            this.textBox_PostalCode.Name = "textBox_PostalCode";
+            this.textBox_PostalCode.Size = new System.Drawing.Size(100, 23);
+            this.textBox_PostalCode.TabIndex = 57;
+            // 
+            // PostalCodelabel
+            // 
+            this.PostalCodelabel.AutoSize = true;
+            this.PostalCodelabel.Location = new System.Drawing.Point(12, 257);
+            this.PostalCodelabel.Name = "PostalCodelabel";
+            this.PostalCodelabel.Size = new System.Drawing.Size(77, 17);
+            this.PostalCodelabel.TabIndex = 56;
+            this.PostalCodelabel.Text = "PostalCode:";
+            // 
+            // textBox_Province
+            // 
+            this.textBox_Province.Location = new System.Drawing.Point(120, 219);
+            this.textBox_Province.Name = "textBox_Province";
+            this.textBox_Province.Size = new System.Drawing.Size(100, 23);
+            this.textBox_Province.TabIndex = 55;
+            // 
+            // Provincelabel
+            // 
+            this.Provincelabel.AutoSize = true;
+            this.Provincelabel.Location = new System.Drawing.Point(12, 219);
+            this.Provincelabel.Name = "Provincelabel";
+            this.Provincelabel.Size = new System.Drawing.Size(60, 17);
+            this.Provincelabel.TabIndex = 54;
+            this.Provincelabel.Text = "Province:";
+            // 
+            // textBox_City
+            // 
+            this.textBox_City.Location = new System.Drawing.Point(120, 180);
+            this.textBox_City.Name = "textBox_City";
+            this.textBox_City.Size = new System.Drawing.Size(100, 23);
+            this.textBox_City.TabIndex = 53;
+            // 
+            // textBox_Address2
+            // 
+            this.textBox_Address2.Location = new System.Drawing.Point(120, 140);
+            this.textBox_Address2.Name = "textBox_Address2";
+            this.textBox_Address2.Size = new System.Drawing.Size(252, 23);
+            this.textBox_Address2.TabIndex = 52;
+            // 
+            // textBox_Address1
+            // 
+            this.textBox_Address1.Location = new System.Drawing.Point(120, 100);
+            this.textBox_Address1.Name = "textBox_Address1";
+            this.textBox_Address1.Size = new System.Drawing.Size(252, 23);
+            this.textBox_Address1.TabIndex = 51;
+            // 
+            // textBox_CompanyName
+            // 
+            this.textBox_CompanyName.Location = new System.Drawing.Point(120, 60);
+            this.textBox_CompanyName.Name = "textBox_CompanyName";
+            this.textBox_CompanyName.Size = new System.Drawing.Size(181, 23);
+            this.textBox_CompanyName.TabIndex = 50;
+            // 
+            // textBox_ClientCode
+            // 
+            this.textBox_ClientCode.Location = new System.Drawing.Point(120, 20);
+            this.textBox_ClientCode.Name = "textBox_ClientCode";
+            this.textBox_ClientCode.Size = new System.Drawing.Size(100, 23);
+            this.textBox_ClientCode.TabIndex = 49;
+            // 
+            // CampanyNameLabel
+            // 
+            this.CampanyNameLabel.AutoSize = true;
+            this.CampanyNameLabel.Location = new System.Drawing.Point(12, 60);
+            this.CampanyNameLabel.Name = "CampanyNameLabel";
+            this.CampanyNameLabel.Size = new System.Drawing.Size(104, 17);
+            this.CampanyNameLabel.TabIndex = 48;
+            this.CampanyNameLabel.Text = "Campany Name:";
+            // 
+            // CityLabel
+            // 
+            this.CityLabel.AutoSize = true;
+            this.CityLabel.Location = new System.Drawing.Point(12, 180);
+            this.CityLabel.Name = "CityLabel";
+            this.CityLabel.Size = new System.Drawing.Size(32, 17);
+            this.CityLabel.TabIndex = 47;
+            this.CityLabel.Text = "City:";
+            // 
+            // Address2Label
+            // 
+            this.Address2Label.AutoSize = true;
+            this.Address2Label.Location = new System.Drawing.Point(12, 140);
+            this.Address2Label.Name = "Address2Label";
+            this.Address2Label.Size = new System.Drawing.Size(66, 17);
+            this.Address2Label.TabIndex = 46;
+            this.Address2Label.Text = "Address2:";
+            // 
+            // Address1Label
+            // 
+            this.Address1Label.AutoSize = true;
+            this.Address1Label.Location = new System.Drawing.Point(12, 100);
+            this.Address1Label.Name = "Address1Label";
+            this.Address1Label.Size = new System.Drawing.Size(66, 17);
+            this.Address1Label.TabIndex = 45;
+            this.Address1Label.Text = "Address1:";
+            // 
+            // ClientCodeLabel
+            // 
+            this.ClientCodeLabel.AutoSize = true;
+            this.ClientCodeLabel.Location = new System.Drawing.Point(12, 20);
+            this.ClientCodeLabel.Name = "ClientCodeLabel";
+            this.ClientCodeLabel.Size = new System.Drawing.Size(74, 17);
+            this.ClientCodeLabel.TabIndex = 44;
+            this.ClientCodeLabel.Text = "ClientCode:";
+            // 
+            // ok_button
+            // 
+            this.ok_button.Location = new System.Drawing.Point(323, 456);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(75, 23);
+            this.ok_button.TabIndex = 63;
+            this.ok_button.Text = "&OK";
+            this.ok_button.UseVisualStyleBackColor = true;
+            this.ok_button.Click += new System.EventHandler(this.ok_button_Click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Location = new System.Drawing.Point(414, 456);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.cancel_button.TabIndex = 64;
+            this.cancel_button.Text = "&Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 493);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.ok_button);
+            this.Controls.Add(this.checkBox_CreditHold);
+            this.Controls.Add(this.textBox_Notes);
+            this.Controls.Add(this.Noteslabel);
+            this.Controls.Add(this.textBox_YTDSales);
+            this.Controls.Add(this.YTDSaleslabel);
+            this.Controls.Add(this.textBox_PostalCode);
+            this.Controls.Add(this.PostalCodelabel);
+            this.Controls.Add(this.textBox_Province);
+            this.Controls.Add(this.Provincelabel);
+            this.Controls.Add(this.textBox_City);
+            this.Controls.Add(this.textBox_Address2);
+            this.Controls.Add(this.textBox_Address1);
+            this.Controls.Add(this.textBox_CompanyName);
+            this.Controls.Add(this.textBox_ClientCode);
+            this.Controls.Add(this.CampanyNameLabel);
+            this.Controls.Add(this.CityLabel);
+            this.Controls.Add(this.Address2Label);
+            this.Controls.Add(this.Address1Label);
+            this.Controls.Add(this.ClientCodeLabel);
+            this.Name = "ClientForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CheckBox checkBox_CreditHold;
+        private TextBox textBox_Notes;
+        private Label Noteslabel;
+        private TextBox textBox_YTDSales;
+        private Label YTDSaleslabel;
+        private TextBox textBox_PostalCode;
+        private Label PostalCodelabel;
+        private TextBox textBox_Province;
+        private Label Provincelabel;
+        private TextBox textBox_City;
+        private TextBox textBox_Address2;
+        private TextBox textBox_Address1;
+        private TextBox textBox_CompanyName;
+        private TextBox textBox_ClientCode;
+        private Label CampanyNameLabel;
+        private Label CityLabel;
+        private Label Address2Label;
+        private Label Address1Label;
+        private Label ClientCodeLabel;
+        private Button ok_button;
+        private Button cancel_button;
+    }
+}
---
+++ COMP3602Assign06/ClientForm.cs
@@ -0,0 +1,69 @@
+﻿using BusinessLib.Common;
+
+namespace COMP3602Assign06
+{
+    public partial class ClientForm : Form
+    {
+        private Client _client;
+        private ErrorProvider errorProvider1 = new ErrorProvider();
+
+        public ClientForm()
+        {
+            InitializeComponent();
+        }
+
+        public ClientForm(Client client, bool isEditMode) : this()
+        {
+            _client = client;
+            textBox_ClientCode.Enabled = !isEditMode;
+
+            textBox_ClientCode.Text = _client.ClientCode;
+            textBox_CompanyName.Text = _client.CompanyName;
+            textBox_Address1.Text = _client.Address1;
+            textBox_Address2.Text = _client.Address2;
+            textBox_City.Text = _client.City;
+            textBox_Province.Text = _client.Province;
+            textBox_PostalCode.Text = _client.PostalCode;
+            textBox_YTDSales.Text = _client.YTDSales.ToString("N2");
+            checkBox_CreditHold.Checked = _client.CreditHold;
+            textBox_Notes.Text = _client.Notes;
+
+            if (isEditMode)
+            {
+                this.ActiveControl = textBox_CompanyName;
+            }
+        }
+
+        private void ok_button_Click(object sender, EventArgs e)
+        {
+            _client.ClientCode = textBox_ClientCode.Text;
+            _client.CompanyName = textBox_CompanyName.Text;
+            _client.Address1 = textBox_Address1.Text;
+            _client.Address2 = textBox_Address2.Text;
+            _client.City = textBox_City.Text;
+            _client.Province = textBox_Province.Text;
+            _client.PostalCode = textBox_PostalCode.Text;
+            _client.YTDSales = decimal.TryParse(textBox_YTDSales.Text, out decimal sales) ? sales : 0;
+            _client.CreditHold = checkBox_CreditHold.Checked;
+            _client.Notes = textBox_Notes.Text;
+
+            var validator = new BusinessLib.Business.ClientValidation();
+            if (validator.Validate(_client))
+            {
+                this.DialogResult = DialogResult.OK; // Set the dialog result to OK
+                this.Close(); // Close the dialog
+            }
+            else
+            {
+                errorProvider1.SetError(ok_button, validator.ErrorMessage);
+            }
+        }
+
+
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel; // Set the dialog result to Cancel
+            this.Close(); // Close the dialog without saving
+        }
+    }
+}
